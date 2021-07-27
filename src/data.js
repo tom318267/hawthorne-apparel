6425289cebc590d9506fd569d155d2498609d85d
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 15:55:55 -0400

    add more items to new section

--- src/data.js
+++ src/data.js
@@ -5,6 +5,11 @@ export const ALL_NEW_DATA = [
     img: "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
+    name: "Red Dress",
+    price: 50,
+    img: "https://images.unsplash.com/photo-1572804013427-4d7ca7268217?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdvbWVuJTIwZHJlc3N8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
+  },
+  {
     name: "Blue Dress Shirt",
     price: 50,
     img: "https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
@@ -13,6 +18,11 @@ export const ALL_NEW_DATA = [
     name: "Leather Jacket",
     price: 80,
     img: "https://images.unsplash.com/photo-1520975954732-35dd22299614?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Yellow Tracksuit",
+    price: 80,
+    img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d29tZW4lMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60",
   },
   {
     name: "Red Jacket",
