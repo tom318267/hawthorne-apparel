7d575a61036313e95f0cc9ea746b48a43729743b
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 13:57:47 -0400

    add all new section

--- src/components/Sale.js
+++ src/components/Sale.js
@@ -1,23 +1,16 @@
 import React from "react";
+import { ALL_NEW_DATA } from "../data";
+import Item from "./Item";
 
 const Sale = () => {
   return (
     <div className="font-quest">
-      <div className="w-full flex">
-        <div className="bg-gray py-6 text-lg w-1/2 flex justify-center">
-          <h2 className="w-1/4 text-center flex">Suits starting at $100</h2>
-        </div>
-        <div className="bg-white py-6 text-lg w-1/2 flex justify-center">
-          <h2 className="w-1/4 text-center flex">Designer suits 20% off</h2>
-        </div>
-      </div>
+      <h2 className="flex text-2xl justify-center pt-8">All New</h2>
 
-      <div className="flex justify-center bg-clearance bg-cover bg-center">
-        <div className="flex justify-center text-xl py-20 bg-overlay w-full h-auto">
-          <h2 className="w-1/3 text-center text-white">
-            Check out our clearance items!
-          </h2>
-        </div>
+      <div className="flex flex-col items-center">
+        {ALL_NEW_DATA.map(({ name, price, img }) => (
+          <Item name={name} price={price} img={img} />
+        ))}
       </div>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const ALL_NEW_DATA = [
+  {
+    name: "Black Suit",
+    price: 100,
+    img: "https://images.unsplash.com/photo-1617127365659-c47fa864d8bc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWVuJTIwZmFzaGlvbnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Blue Dress Shirt",
+    price: 50,
+    img: "https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjZ8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Leather Jacket",
+    price: 80,
+    img: "https://images.unsplash.com/photo-1520975954732-35dd22299614?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzZ8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "Red Jacket",
+    price: 80,
+    img: "https://images.unsplash.com/photo-1552573102-2b44b44d85b5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG1lbiUyMGZhc2hpb258ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+];
