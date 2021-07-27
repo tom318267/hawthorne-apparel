f62dd1eeeb41799e7ee2b96f425e5564a852f801
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 14:32:14 -0400

    add aos to new items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Aos from "aos";
 
 const Item = ({ name, price, img }) => {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
-    <div class="p-10">
-      <div class="max-w-xs rounded overflow-hidden shadow-lg font-quest">
-        <img class="w-full" src={img} alt="Mountain" />
-        <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">{name}</div>
-          <p class="text-gray-700 text-base mb-2">
+    <div data-aos="fade-up" className="p-10">
+      <div className="max-w-xs rounded overflow-hidden shadow-lg font-quest">
+        <img className="w-full" src={img} alt="Mountain" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{name}</div>
+          <p className="text-gray-700 text-base mb-2">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
