4623a0ba8e6a305335de83b378f4823dde4bcd64
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 00:56:42 -0400

    adjust sale component

--- src/components/Sale.js
+++ src/components/Sale.js
@@ -5,15 +5,15 @@ const Sale = () => {
     <div className="font-quest">
       <div className="w-full flex">
         <div className="bg-gray-500 py-6 text-lg w-1/2 flex justify-center">
-          <h2 className="w-1/4 text-center">Suits starting at $100</h2>
+          <h2 className="w-1/4 text-center flex">Suits starting at $100</h2>
         </div>
-        <div className="bg-white py-6 w-1/2 flex text-lg justify-center">
-          <h2 className="w-1/4 text-center">Designer suits 20% off</h2>
+        <div className="bg-white py-6 text-lg w-1/2 flex justify-center">
+          <h2 className="w-1/4 text-center flex">Designer suits 20% off</h2>
         </div>
       </div>
 
       <div className="flex justify-center bg-clearance bg-cover bg-center">
-        <div className="flex justify-center text-lg py-20 bg-overlay w-full h-auto">
+        <div className="flex justify-center text-xl py-20 bg-overlay w-full h-auto">
           <h2 className="w-1/3 text-center text-white">
             Check out our clearance items!
           </h2>
