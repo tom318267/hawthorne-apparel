df8fcd0d628478cecab0401b7e48ac9669bcac45
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 21:09:35 -0400

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,23 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black w-full flex font-quest">
-      <div className="text-white flex flex-col p-8 list-none">
+    <div className="bg-black w-full flex justify-evenly font-quest text-white">
+      <div className="flex flex-col p-8 list-none">
         <li>Home</li>
-        <li>About</li>
-        <li>Home</li>
+        <li>About Us</li>
+        <li>Careers</li>
+        <li>Contact</li>
       </div>
-      <div className="text-white flex flex-col p-8 list-none">
+      <div className="flex flex-col p-8 list-none">
         <li>Facebook</li>
-        <li>About</li>
-        <li>Home</li>
+        <li>Instagram</li>
+        <li>Twitter</li>
       </div>
-      <div className="text-white flex flex-col p-8 list-none">
-        <li>Home</li>
-        <li>About</li>
-        <li>Home</li>
+      <div className="flex flex-col p-8 list-none">
+        <li>Terms of Use</li>
+        <li>Affiliates</li>
+        <li>Private Policy</li>
+        <li>Support</li>
       </div>
     </div>
   );
