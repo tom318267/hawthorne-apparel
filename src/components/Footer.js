cf4da0577e91e539864f9b03456473d460d64440
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 21:22:15 -0400

    adjust footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,24 +2,29 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-black w-full flex justify-evenly font-quest text-white">
-      <div className="flex flex-col p-8 list-none">
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Careers</li>
-        <li>Contact</li>
+    <div>
+      <div className="bg-black w-full flex justify-evenly py-12 font-quest text-white text-md footer-container">
+        <div className="flex flex-col list-none">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Careers</li>
+          <li>Contact</li>
+        </div>
+        <div className="flex flex-col list-none">
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </div>
+        <div className="flex flex-col list-none">
+          <li>Terms of Use</li>
+          <li>Affiliates</li>
+          <li>Private Policy</li>
+          <li>Support</li>
+        </div>
       </div>
-      <div className="flex flex-col p-8 list-none">
-        <li>Facebook</li>
-        <li>Instagram</li>
-        <li>Twitter</li>
-      </div>
-      <div className="flex flex-col p-8 list-none">
-        <li>Terms of Use</li>
-        <li>Affiliates</li>
-        <li>Private Policy</li>
-        <li>Support</li>
-      </div>
+      <h2 className="bg-black text-white text-center pb-4 text-xs sm:text-sm">
+        @ 2021 Hawthorne Apparel Co. All Rights Reserved.
+      </h2>
     </div>
   );
 };
