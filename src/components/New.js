5cc39bcaa8b16f66241d292181a2212c597b61c4
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-27 21:24:50 -0400

    adjust footer

--- src/components/New.js
+++ src/components/New.js
@@ -4,7 +4,7 @@ import Item from "./Item";
 
 const New = () => {
   return (
-    <div className="font-quest">
+    <div className="font-quest py-8">
       <h2 className="flex text-2xl justify-center font-noto pt-14">All New</h2>
       <div className="flex flex-col items-center sm:flex-row sm:flex-wrap sm:items-stretch sm:justify-center">
         {ALL_NEW_DATA.map(({ name, price, img }) => (
